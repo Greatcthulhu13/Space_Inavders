268e2cb203557597072864e97665d406353fcd83
Guffy <144727889+Greatcthulhu13@users.noreply.github.com>
2023-10-14 13:58:41 +0100

    Update game.py

--- game.py
+++ game.py
@@ -10,9 +10,9 @@ WIDTH, HEIGHT = 800, 600
 PLAYER_SIZE = 50
 PLAYER_SPEED = 5
 ENEMY_SIZE = 50
-ENEMY_SPEED = 2
 BULLET_SIZE = 10
 BULLET_SPEED = 10
+PLAYER_HEALTH = 100
 
 # Colors
 WHITE = (255, 255, 255)
@@ -30,7 +30,7 @@ bullet_img = pygame.image.load("bullet.png")
 
 # Set up player
 player_x = WIDTH // 2 - PLAYER_SIZE // 2
-player_y = HEIGHT  - 3 * PLAYER_SIZE
+player_y = HEIGHT - 3 * PLAYER_SIZE
 player_x_change = 0
 
 # Set up enemy
@@ -53,6 +53,13 @@ font = pygame.font.Font(None, 36)
 # Game over text
 game_over_text = font.render("Game Over", True, RED)
 
+# Constants for levels
+LEVELS = [
+    {"enemy_speed": 2, "num_enemies": 5, "score_threshold": 5},
+    {"enemy_speed": 3, "num_enemies": 7, "score_threshold": 10},
+]
+current_level = 0
+
 def player(x, y):
     screen.blit(player_img, (x, y))
 
@@ -61,7 +68,7 @@ def enemy(x, y):
 
 def fire_bullet(x, y):
     global bullet_state
-    bullet_state = 0 
+    bullet_state = 0
     screen.blit(bullet_img, (x + PLAYER_SIZE // 2 - BULLET_SIZE // 2, y - BULLET_SIZE))
 
 def is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
@@ -69,26 +76,6 @@ def is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
     if distance < (ENEMY_SIZE + BULLET_SIZE) / 2:
         return True
     return False
-
-# Constants for levels
-LEVELS = [
-    {"enemy_speed": 2, "num_enemies": 5, "player_health": 100, "score_threshold": 5},
-    {"enemy_speed": 3, "num_enemies": 7, "player_health": 90, "score_threshold": 10},
-]
-current_level = 0
-
-# Game loop
-running = True
-while running:
-    # ... (existing game loop code) ...
-
-    # Check for advancing to the next level
-    if score >= LEVELS[current_level]["score_threshold"]:
-        current_level += 1
-        if current_level < len(LEVELS):
-            enemy_speed = LEVELS[current_level]["enemy_speed"]
-            enemy_x = random.randint(0, WIDTH - ENEMY_SIZE)
-            enemy_y = random.randint(50, 150)
 
 # Game loop
 running = True
@@ -125,22 +112,22 @@ while running:
         player_x = WIDTH - PLAYER_SIZE
 
     # Update enemy's position
-    enemy_x += ENEMY_SPEED
+    enemy_x += LEVELS[current_level]["enemy_speed"]
 
     # Enemy boundary checking
     if enemy_x < 0:
         enemy_x = 0
-        ENEMY_SPEED *= -1
+        LEVELS[current_level]["enemy_speed"] *= -1
         enemy_y += ENEMY_SIZE
     elif enemy_x > WIDTH - ENEMY_SIZE:
         enemy_x = WIDTH - ENEMY_SIZE
-        ENEMY_SPEED *= -1
+        LEVELS[current_level]["enemy_speed"] *= -1
         enemy_y += ENEMY_SIZE
 
     # Bullet movement
-    if bullet_state == 2:
+    if bullet_state == 0:
         fire_bullet(bullet_x, bullet_y)
-        bullet_y += bullet_y_change
+        bullet_y -= bullet_y_change
 
     # Bullet reset
     if bullet_y <= 0:
