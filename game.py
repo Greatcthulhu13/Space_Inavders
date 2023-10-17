49d0cd9f92a652e7972d51cc2adbe1dea2f41f05
Guffy <144727889+Greatcthulhu13@users.noreply.github.com>
2023-10-17 09:34:12 +0100

    Update game.py

--- game.py
+++ game.py
@@ -36,13 +36,14 @@ player_x_change = 0
 # Set up enemy
 enemy_x = random.randint(0, WIDTH - ENEMY_SIZE)
 enemy_y = random.randint(50, 150)
+enemy_speed = 2
 
 # Set up bullet
 bullet_x = 0
 bullet_y = HEIGHT - PLAYER_SIZE
 bullet_x_change = 0
 bullet_y_change = BULLET_SPEED
-bullet_state = 1  # 1 means the bullet is ready to fire
+bullet_state = "ready"  # "ready" or "fire"
 
 # Score
 score = 0
@@ -50,8 +51,9 @@ score = 0
 # Font
 font = pygame.font.Font(None, 36)
 
-# Game over text
-game_over_text = font.render("Game Over", True, RED)
+# Load sounds
+bullet_sound = pygame.mixer.Sound("laser.ogg")
+explosion_sound = pygame.mixer.Sound("explosion.ogg")
 
 # Constants for levels
 LEVELS = [
@@ -61,19 +63,19 @@ LEVELS = [
 current_level = 0
 
 def player(x, y):
-    screen.blit(player_img, (x, y))
+    screen.blit(player_img, (x, y)
 
 def enemy(x, y):
-    screen.blit(enemy_img, (x, y))
+    screen.blit(enemy_img, (x, y)
 
 def fire_bullet(x, y):
     global bullet_state
-    bullet_state = 0
+    bullet_state = "fire"
     screen.blit(bullet_img, (x + PLAYER_SIZE // 2 - BULLET_SIZE // 2, y - BULLET_SIZE))
 
 def is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
-    distance = ((enemy_x - bullet_x) ** 2 + (enemy_y - bullet_y) ** 2) ** 0.5
-    if distance < (ENEMY_SIZE + BULLET_SIZE) / 2:
+    distance_squared = (enemy_x - bullet_x) ** 2 + (enemy_y - bullet_y) ** 2
+    if distance_squared < ((ENEMY_SIZE + BULLET_SIZE) / 2) ** 2:
         return True
     return False
 
@@ -91,8 +93,7 @@ while running:
             if event.key == pygame.K_RIGHT:
                 player_x_change = PLAYER_SPEED
             if event.key == pygame.K_SPACE:
-                if bullet_state == 1:
-                    bullet_sound = pygame.mixer.Sound("laser.ogg")
+                if bullet_state == "ready":
                     bullet_sound.play()
                     bullet_x = player_x
                     fire_bullet(bullet_x, bullet_y)
@@ -106,40 +107,29 @@ while running:
     player_x += player_x_change
 
     # Ensure player doesn't go out of bounds
-    if player_x < 0:
-        player_x = 0
-    elif player_x > WIDTH - PLAYER_SIZE:
-        player_x = WIDTH - PLAYER_SIZE
+    player_x = max(0, min(player_x, WIDTH - PLAYER_SIZE))
 
     # Update enemy's position
-    enemy_x += LEVELS[current_level]["enemy_speed"]
+    enemy_x += enemy_speed
 
     # Enemy boundary checking
-    if enemy_x < 0:
-        enemy_x = 0
-        LEVELS[current_level]["enemy_speed"] *= -1
-        enemy_y += ENEMY_SIZE
-    elif enemy_x > WIDTH - ENEMY_SIZE:
-        enemy_x = WIDTH - ENEMY_SIZE
-        LEVELS[current_level]["enemy_speed"] *= -1
+    if enemy_x < 0 or enemy_x > WIDTH - ENEMY_SIZE:
+        enemy_speed = -enemy_speed
         enemy_y += ENEMY_SIZE
 
     # Bullet movement
-    if bullet_state == 0:
+    if bullet_state == "fire":
         fire_bullet(bullet_x, bullet_y)
         bullet_y -= bullet_y_change
 
     # Bullet reset
     if bullet_y <= 0:
-        bullet_y = HEIGHT - PLAYER_SIZE
-        bullet_state = 1
+        bullet_state = "ready"
 
     # Collision
     if is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
-        explosion_sound = pygame.mixer.Sound("explosion.ogg")
         explosion_sound.play()
-        bullet_y = HEIGHT - PLAYER_SIZE
-        bullet_state = 1
+        bullet_state = "ready"
         score += 1
         enemy_x = random.randint(0, WIDTH - ENEMY_SIZE)
         enemy_y = random.randint(50, 150)
